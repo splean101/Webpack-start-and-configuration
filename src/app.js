468807b9a435ab7f21126ab5b08a0a27b5121ab4
Vitalii Hubariev <101splean@gmail.com>
2019-07-14 14:45:46 +0300

    finaly

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-import {one} from './script1';
-import {sayHi} from './script2';
+import { one } from './script1';
+import { sayHi } from './script2';
 
 console.log(one);
 sayHi();
+/*import hello from "./hello.html";
+
+console.log(hello);*/
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-module.exports = {
-    entry: "./src/app.js"
- };
- const path = require("path");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-   output: {
-      path: path.resolve(__dirname, "public"),
-      filename: "bundle.js"
-   }
+    entry: "./src/app.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+           { test: /\.html$/, use: "html-loader" }
+        ]
+     },
+     plugins: [
+        new HtmlWebpackPlugin({template: "./src/index.html"})
+      ],
+      mode: "development"  
 };
+
+
